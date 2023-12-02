d231299bf00c5e34fc46f3c49a1c71581ee4d92f
thenhanDI21V7X336 <130239127+thenhanDI21V7X336@users.noreply.github.com>
2023-12-02 13:10:13 +0700

    Cai dat express chay thong bao xin chao

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const cors = require("cors");
+const res = require("express/lib/response");
+const req = require("express/lib/request");
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({message: "Wellcom to contact book application." });
+});
+
+module.exports = app;
