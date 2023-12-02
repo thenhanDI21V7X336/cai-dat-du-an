b9915c0c96d755cfa60cab22affc9c9619df4ff2
thenhanDI21V7X336 <130239127+thenhanDI21V7X336@users.noreply.github.com>
2023-12-02 18:53:32 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const res = require("express/lib/response");
 const req = require("express/lib/request");
 const contactsRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -16,5 +17,18 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+// Code ở đây sẽ chạy khi không có route được định nghĩa nào khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+app.use((err, req, res, next) => {
+// Trong các đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
+
 
 module.exports = app;
